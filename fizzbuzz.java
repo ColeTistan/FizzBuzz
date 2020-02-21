c1dbdcb7931ff48496037ed098f9ce880ee520f5
Cole Tistan <tistanc1@students.rowan.edu>
2020-02-20 20:11:53 -0500

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,19 +1,21 @@
-public static void main(String[] args) {
-    for(int i = 1; i <= 100; i++) {
-        if(i % 3 == 0) {
-            System.out.print("fizz\n");
-        }
-        
-        else if(i % 5 == 0) {
-            System.out.print("buzz\n");
-        }
+class fizzbuzz {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            if(i % 3 == 0) {
+                System.out.print("fizz\n");
+            }
 
-        else if(i % 3 == 0 && i % 5 == 0) {
-            System.out.print("fizzbuzz\n");
-        }
+            else if(i % 5 == 0) {
+                System.out.print("buzz\n");
+            }
 
-        else {
-            System.out.print(i + "\n");
+            else if(i % 3 == 0 && i % 5 == 0) {
+                System.out.print("fizzbuzz\n");
+            }
+
+            else {
+                System.out.print(i + "\n");
+            }
         }
     }
 }
